eb2c37f273a4fae131bf288e7260a1a668650203
Heiko Westermann <h.westermann@windguard.de>
2020-05-20 10:52:08 +0200

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='SL1oPhoton',
-      version='0.1.2',
+      version='0.1.3',
       # packages=['pyphotonfile'],
       author="Heiko Westermann",
       author_email="heiko+sl1tophoton@orgizm.net",
